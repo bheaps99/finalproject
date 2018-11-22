fde5f35eaefcd7529d7c39958571d5b56049f30c
bheaps99 <44827547+bheaps99@users.noreply.github.com>
2018-11-21 16:15:58 -0800

    Delete the reponsive nav script from an old sub fork repo

--- finalproject_will_work/scripts/show-nav.js
+++
@@ -1,7 +0,0 @@
-// Show Mobile Navigation
-const body = document.body;
-const btn = document.querySelector('.hamburger');
-
-btn.addEventListener('click', function(){
-    body.classList.toggle('show');
-});
